82302741b0486d70fb3842768824cb710867b47a
Maetarenn <hmakiferen12@gmail.com>
2025-05-11 21:24:08 +0300

    Initial commit

---
+++ new.py
@@ -0,0 +1,18 @@
+ilksayı = int(input("İlk sayıyı giriniz."))
+ikincisayı = int(input("İkinci sayıyı giriniz."))
+islem = input("""Yapmak istediğiniz işlemi giriniz.
+(Toplama: +, Çıkarma: -, Çarpma: *, Bölme: /,)                  
+""")
+if islem == "+":
+    print("Sonuç:"+str(ilksayı+ikincisayı))
+
+elif islem == "-":
+    print("Sonuç"+str(ilksayı+ikincisayı))
+
+elif islem == "*":
+    print("Sonuç:"+str(ilksayı+ikincisayı))
+
+elif islem == "/":
+    print("Sonuç:"+str(ilksayı+ikincisayı))
+
+
